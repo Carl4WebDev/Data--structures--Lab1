7e99385d98efd3937d92dc9b1769f3e72df39dba
Carl Ivan Sampan <carlforwebdev@gmail.com>
2024-08-22 12:20:35 +0800

    another

--- main.py
+++ main.py
@@ -5,13 +5,11 @@ first_number = int(number1)
 second_number = int(number2)
 
 if first_number < second_number:
-    print(str(first_number) + " is less than " + str(second_number))
-elif second_number > first_number:
-    print(str(second_number) + " is greater than " + str(first_number))
+    print(f"{ first_number } is less than { second_number }")
+elif first_number > second_number:
+    print(f"{ first_number } is greater than { second_number }")
 elif first_number == second_number:
-    print(str(first_number) + "is equal to " + str(second_number))
-else:
-    print("Invalid Input")
+    print(str(first_number) + " is equal to " + str(second_number))
 
 # classmate1 = input("Enter classmate name 1: ")
 # classmate2 = input("Enter classmate name 2: ")
